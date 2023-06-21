7d0eb0ee2d3ffe25be60af070d28924cfdee0b23
ItsVasconcellos <fernando.asv@uol.com.br>
2023-06-21 17:49:57 -0300

    feat: Atualizar Status do ticket

--- src/backend/routes/tickets.js
+++ src/backend/routes/tickets.js
@@ -84,10 +84,12 @@ router.post("/criar-ticket", (req, res) => {
             });
 }});
 
-router.post("/criar-ticket", (req, res) => {
+router.post("/status", (req, res) => {
     //Verifica se o usuário está logado
     if (req.session.autenticado) {
         if(req.session.id_user==1){
+            var id = req.body.id_ticket;
+            var status = req.body.status;
             //Garantir que a requisição tem código inicial correto
             res.statusCode = 200;
             //Define o cabeçalho da requisição
@@ -95,8 +97,57 @@ router.post("/criar-ticket", (req, res) => {
             //Inicializa o banco de dados
             var db = new sqlite3.Database(DBPATH); 
             //Comando sql a ser executado
+            var sql = `UPDATE Tickets SET STATUS = ${status} WHERE Tickets = ${id}`;
+            db.all(sql,[],(err,rows)=>{
+                if (err) {
+                    //Joga o erro pro console, impedindo acontecer um travamento geral
+                    throw err;
+                }
+                res.redirect("/ticket");
+                db.close();
+            });
         }
 }});
 
+router.post("/aprovado", (req, res) => {
+    //Verifica se o usuário está logado
+    if (req.session.autenticado) {
+        if(req.session.id_user==1){
+            var id = req.body.id_ticket;
+            var aprovado = req.body.aprovado;
+            //Garantir que a requisição tem código inicial correto
+            res.statusCode = 200;
+            //Define o cabeçalho da requisição
+            res.setHeader('Acess-Control-Allow-Origin', '*');
+            //Inicializa o banco de dados
+            var db = new sqlite3.Database(DBPATH); 
+            //Comando sql a ser executado
+            var sql = `UPDATE Tickets SET APROVADO = ${aprovado} WHERE ID_TICKET = ${id}`;
+            db.all(sql,[],(err)=>{
+                if (err) {
+                    //Joga o erro pro console, impedindo acontecer um travamento geral
+                    throw err;
+                }
+                var sqlTicket = `SELECT * FROM Tickets WHERE ID_TICKET = ${id}`;
+                db.all(sqlTicket,[],(err,rows)=>{
+                    if (err) {
+                        //Joga o erro pro console, impedindo acontecer um travamento geral
+                        throw err;
+                    }
+                    var atualizar = `UPDATE Catalogo_Dados_Tabelas SET DATABASE = ${rows[0].DATABASE}, TABELA = ${rows[0].TABELA}, CAMINHO = ${rows[0].CAMINHO}`
+                    db.run(atualizar,[],err=>{
+                        if (err) {
+                            //Joga o erro pro console, impedindo acontecer um travamento geral
+                            throw err;
+                        }  
+                        res.redirect("/ticket");
+                        db.close();
+                    })
+                })
+            });
+            db.close();
+        }
+    }
+});
 
 module.exports = router;
